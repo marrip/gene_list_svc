c481f8d08aa2b0a685aa53a23992f9025612158c
marrip <martin.rippin@scilifelab.uu.se>
2021-11-09 16:31:40 +0100

    Feat: Define some global vars

---
+++ constants.go
@@ -0,0 +1,5 @@
+package main
+
+var tables = map[string]string{
+	"entity": "id varchar(20) NOT NULL, class varchar(10) NOT NULL, snv boolean, cnv boolean, sv boolean, pindel boolean, list_aml boolean, list_aml_ext boolean, list_all boolean, PRIMARY KEY (id)",
+}
