b0e68da77e3bd8a20e429036f42339a052308cd1
marrip <martin.rippin@scilifelab.uu.se>
2021-11-30 16:05:29 +0100

    Feat: Update constants and add build infos

--- constants.go
+++ constants.go
@@ -7,42 +7,47 @@ var analyses = []string{
 	"pindel",
 }
 
+var builds = []string{
+	"37",
+	"38",
+}
+
 var lists = []string{
 	"aml",
 	"aml_ext",
 	"all",
 }
 
-var chromosomes = map[string]bool{
-	"1":  true,
-	"2":  true,
-	"3":  true,
-	"4":  true,
-	"5":  true,
-	"6":  true,
-	"7":  true,
-	"8":  true,
-	"9":  true,
-	"10": true,
-	"11": true,
-	"12": true,
-	"13": true,
-	"14": true,
-	"15": true,
-	"16": true,
-	"17": true,
-	"18": true,
-	"19": true,
-	"20": true,
-	"21": true,
-	"22": true,
-	"X":  true,
-	"Y":  true,
+var chromosomes = []string{
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
+	"15",
+	"16",
+	"17",
+	"18",
+	"19",
+	"20",
+	"21",
+	"22",
+	"X",
+	"Y",
 }
 
 var unknownIds = map[string]string{
-	"AFND":           "ENSG00000130396",
-	"RP11-1407O15.2": "ENSG00000174093",
-	"SEPT5-GP1BB":    "ENSG00000284874",
-	"TMX2-CTNND1":    "ENSG00000288534",
+	"AFND":        "ENSG00000130396",
+	"CSFR3":       "ENSG00000119535",
+	"SEPT5-GP1BB": "ENSG00000284874",
+	"TMX2-CTNND1": "ENSG00000288534",
 }
