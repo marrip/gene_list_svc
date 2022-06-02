0087a5f905665c2d7a66bbaa21f56b78b01d9603
marrip <martin.rippin@scilifelab.uu.se>
2022-06-02 15:33:17 +0200

    feat: Add functions to retrieve coords and write bed file

--- cmd/db_to_tsv.go
+++ cmd/db_to_tsv.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func dbToTsv() (err error) {
@@ -11,79 +13,233 @@ func dbToTsv() (err error) {
 	if err != nil {
 		return
 	}
-	var regions []DbTableRow
-	for _, row := range rows {
-		if row.Class == "region" {
-			regions = append(regions, row)
-			continue
+	var regions []EnsemblBaseObj
+	switch session.Analysis {
+	case "pindel", "sv":
+		regions, err = prepForPindelSv(rows)
+		if err != nil {
+			return
 		}
-		switch session.Analysis {
-		case "pindel", "sv":
-			err = row.getCompleteRegion()
-			if err != nil {
-				continue
-			}
-			regions = append(regions, row)
-		case "cnv", "snv":
-			if row.Class == "exon" {
-				err = row.getCompleteRegion()
-				if err != nil {
-					continue
-				}
-				regions = append(regions, row)
-			} else {
-			}
+	case "cnv", "snv":
+		regions, err = prepForCnvSnv(rows)
+		if err != nil {
+			return
 		}
 	}
-	fmt.Println(regions)
-	fmt.Println(len(regions))
+	if err = regionsToTsv(regions); err != nil {
+		return
+	}
 	return
 }
 
-func (d *DbTableRow) getCompleteRegion() (err error) {
+func prepForPindelSv(rows []DbTableRow) (regions []EnsemblBaseObj, err error) {
+	for _, row := range rows {
+		var region EnsemblBaseObj
+		if row.Class == "region" {
+			region, err = row.rowToRegion()
+			if err != nil {
+				return
+			}
+		} else {
+			region, err = row.getCompleteRegion(50)
+			if err != nil {
+				return
+			}
+		}
+		regions = append(regions, region)
+	}
+	return
+}
+
+func (d DbTableRow) rowToRegion() (region EnsemblBaseObj, err error) {
+	region.Annotation = d.Id
+	region.Chromosome = d.Chromosome
+	if region.Start, err = strconv.Atoi(d.Start); err != nil {
+		return
+	}
+	if region.End, err = strconv.Atoi(d.End); err != nil {
+		return
+	}
+	return
+}
+
+func (d DbTableRow) getCompleteRegion(size int) (region EnsemblBaseObj, err error) {
 	body, err := d.getCoordinates(false)
 	if err != nil {
 		return
 	}
-	var obj EnsemblBaseObj
-	json.Unmarshal(body, &obj)
-	d.Chromosome = obj.Chromosome
-	d.Start = strconv.Itoa(obj.Start)
-	d.End = strconv.Itoa(obj.End)
+	json.Unmarshal(body, &region)
+	if d.Id != region.EnsemblId {
+		region.Annotation = fmt.Sprintf("%s|%s", d.Id, region.EnsemblId)
+	} else {
+		region.Annotation = d.Id
+	}
+	region.addWindow(size)
 	return
 }
 
-func (d DbTableRow) getExons() (regions []DbTableRow, err error) {
+func (o *EnsemblBaseObj) addWindow(size int) {
+	o.Start = o.Start - size
+	o.End = o.End + size
+}
+
+func prepForCnvSnv(rows []DbTableRow) (regions []EnsemblBaseObj, err error) {
+	for _, row := range rows {
+		var region EnsemblBaseObj
+		if row.Class == "region" {
+			region, err = row.rowToRegion()
+			if err != nil {
+				return
+			}
+			regions = append(regions, region)
+		} else if row.Class == "exon" {
+			region, err = row.getCompleteRegion(10)
+			if err != nil {
+				return
+			}
+			regions = append(regions, region)
+		} else {
+			var exons []EnsemblBaseObj
+			exons, err = row.getExons(10)
+			if err != nil {
+				return
+			}
+			regions = append(regions, exons...)
+		}
+	}
+	return
+}
+
+func (d DbTableRow) getExons(size int) (regions []EnsemblBaseObj, err error) {
 	body, err := d.getCoordinates(true)
 	if err != nil {
 		return
 	}
-	var exons []EnsemblBaseObj
 	if d.Class == "gene" {
 		var obj EnsemblGeneObj
 		json.Unmarshal(body, &obj)
+		var exons []EnsemblBaseObj
 		for _, transcript := range obj.Transcripts {
 			for _, exon := range transcript.Exons {
-				exon.Transcript = []string{transcript.EnsemblId}
+				exon.Transcript = transcript.EnsemblId
 				exons = append(exons, exon)
 			}
 		}
+		geneAnnotation := fmt.Sprintf("%s|%s", obj.Id, obj.EnsemblId)
+		regions = append(regions, uniqueExons(geneAnnotation, exons)...)
 	} else if d.Class == "transcript" {
 		var obj EnsemblTransObj
 		json.Unmarshal(body, &obj)
-		exons = obj.Exons
+		for _, exon := range obj.Exons {
+			exon.addWindow(size)
+			exon.Annotation = fmt.Sprintf("%s|%s|%s", d.Id, obj.EnsemblId, exon.EnsemblId)
+			regions = append(regions, exon)
+		}
 	}
-	exons = uniqueExons(exons)
 	return
 }
 
-func uniqueExons(exons []EnsemblBaseObj) (uniqueExons []EnsemblBaseObj) {
-	keys := make(map[string]bool)
+func uniqueExons(annotation string, exons []EnsemblBaseObj) (uniqueExons []EnsemblBaseObj) {
+	uniqs := make(map[string]EnsemblBaseObj)
 	for _, exon := range exons {
-		if !keys[exon.EnsemblId] {
-			keys[exon.EnsemblId] = true
-			uniqueExons = append(uniqueExons, exon)
+		if _, present := uniqs[exon.EnsemblId]; !present {
+			exon.addWindow(10)
+			exon.Annotation = fmt.Sprintf("%s|%s|%s", annotation, exon.Transcript, exon.EnsemblId)
+			uniqs[exon.EnsemblId] = exon
+		} else {
+			uniq := uniqs[exon.EnsemblId]
+			exonAnnotation := strings.Split(uniq.Annotation, "|")
+			uniq.Annotation = fmt.Sprintf("%s|%s|%s&%s|%s", exonAnnotation[0], exonAnnotation[1], exonAnnotation[2], exon.Transcript, exonAnnotation[3])
+			uniqs[exon.EnsemblId] = uniq
 		}
 	}
+	for _, uniq := range uniqs {
+		uniqueExons = append(uniqueExons, uniq)
+	}
+	return
+}
+
+func regionsToTsv(regions []EnsemblBaseObj) (err error) {
+	regions = sortRegions(regions)
+	lines := regionsToSlices(regions)
+	err = writeTsv(session.Bed, lines)
+	return
+}
+
+func sortRegions(regions []EnsemblBaseObj) (sortedRegions []EnsemblBaseObj) {
+	sort.SliceStable(regions, func(i, j int) bool { return regions[i].End < regions[j].End })
+	sort.SliceStable(regions, func(i, j int) bool { return regions[i].Start < regions[j].Start })
+	sortedRegions = sortChromosomes(regions)
+	return
+}
+
+func sortChromosomes(regions []EnsemblBaseObj) (sortedRegions []EnsemblBaseObj) {
+	chromosomes := generateChromosomeSlice()
+	for _, chromosome := range chromosomes {
+		for _, region := range regions {
+			if region.Chromosome == chromosome {
+				sortedRegions = append(sortedRegions, region)
+			}
+		}
+	}
+	return
+}
+
+func generateChromosomeSlice() (chromosomes []string) {
+	for i := 1; i <= 22; i++ {
+		chromosomes = append(chromosomes, strconv.Itoa(i))
+	}
+	for _, chromosome := range []string{"X", "Y", "M"} {
+		chromosomes = append(chromosomes, chromosome)
+	}
+	return
+}
+
+func regionsToSlices(regions []EnsemblBaseObj) (lines [][]string) {
+	var overlapRegions []EnsemblBaseObj
+	for i, region := range regions {
+		if i == 0 {
+			overlapRegions = []EnsemblBaseObj{region}
+		} else if identifyOverlap(regions[i-1], region) {
+			overlapRegions = append(overlapRegions, region)
+		} else {
+			lines = append(lines, mergeOverlappingRegions(overlapRegions))
+			overlapRegions = []EnsemblBaseObj{region}
+		}
+	}
+	return
+}
+
+func identifyOverlap(a, b EnsemblBaseObj) bool {
+	if a.Chromosome == b.Chromosome && a.End >= b.Start {
+		return true
+	}
+	return false
+}
+
+func mergeOverlappingRegions(regions []EnsemblBaseObj) (line []string) {
+	var chromosome, annotation string
+	var start, end int
+	for i, region := range regions {
+		if i == 0 {
+			start = region.Start
+			end = region.End
+			annotation = region.Annotation
+		} else {
+			annotation = fmt.Sprintf("%s;%s", annotation, region.Annotation)
+		}
+		if region.Start < start {
+			start = region.Start
+		}
+		if region.End > end {
+			end = region.End
+		}
+	}
+	if session.Chr {
+		chromosome = fmt.Sprintf("chr%s", regions[0].Chromosome)
+	} else {
+		chromosome = regions[0].Chromosome
+	}
+	line = []string{chromosome, strconv.Itoa(start), strconv.Itoa(end), annotation}
 	return
 }
